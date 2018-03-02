344364e24a28f6e126bce8724d8ad86cba77c3bc
ShadyObeyd <obeyd.shady@gmail.com>
2018-03-02 19:56:03 +0200

    Still trying to fix the bug in 8th problem from 10.InterfacesAndAbstraction-Exercise

--- 10.InterfacesAndAbstraction/08.MilitaryElite/StartUp.cs
+++ 10.InterfacesAndAbstraction/08.MilitaryElite/StartUp.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class StartUp
 {
@@ -14,45 +13,79 @@ class StartUp
         {
             string[] soldierTokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            switch (soldierTokens[0].ToLower())
+            switch (soldierTokens[0])
             {
-                case "private":
+                case "Private":
                     Private privatee = new Private(soldierTokens[1], soldierTokens[2], soldierTokens[3], double.Parse(soldierTokens[4]));
                     army.Add(privatee);
                     break;
-                case "leutenantgeneral":
+                case "LeutenantGeneral":
                     LeutenantGeneral officer = new LeutenantGeneral(soldierTokens[1], soldierTokens[2], soldierTokens[3], double.Parse(soldierTokens[4]));
-                    string[] privateIds = soldierTokens.Skip(5).ToArray();
 
-                    AddPrivates(army, officer, privateIds);
+                    if (soldierTokens.Length > 5)
+                    {
+                        for (int i = 5; i < soldierTokens.Length; i++)
+                        {
+                            string privateId = soldierTokens[i];
+
+                            foreach (Soldier soldier in army)
+                            {
+                                if (soldier.Id == privateId)
+                                {
+                                    officer.Privates.Add(soldier);
+                                    break;
+                                }
+                            }
+                        }
+                    }
                     army.Add(officer);
                     break;
-                case "engineer":
+                case "Engineer":
                     if (!CorpsIsValid(soldierTokens[5]))
                     {
                         continue;
                     }
                     Engineer engineer = new Engineer(soldierTokens[1], soldierTokens[2], soldierTokens[3], double.Parse(soldierTokens[4]), soldierTokens[5]);
 
-                    string[] repairs = soldierTokens.Skip(6).ToArray();
+                    if (soldierTokens.Length > 6)
+                    {
+                        for (int i = 6; i < soldierTokens.Length; i+= 2)
+                        {
+                            string partName = soldierTokens[i];
+                            int hoursWorked = int.Parse(soldierTokens[i + 1]);
 
-                    GetRepairs(engineer, repairs);
+                            Repair repair = new Repair(partName, hoursWorked);
+
+                            engineer.Repairs.Add(repair);
+                        }
+                    }
                     army.Add(engineer);
                     break;
-                case "commando":
+                case "Commando":
                     if (!CorpsIsValid(soldierTokens[5]))
                     {
                         continue;
                     }
-
                     Commando commando = new Commando(soldierTokens[1], soldierTokens[2], soldierTokens[3], double.Parse(soldierTokens[4]), soldierTokens[5]);
 
-                    string[] missions = soldierTokens.Skip(6).ToArray();
+                    if (soldierTokens.Length > 6)
+                    {
+                        for (int i = 6; i < soldierTokens.Length; i+= 2)
+                        {
+                            string codeName = soldierTokens[i];
+                            string state = soldierTokens[i + 1];
 
-                    GetMissions(commando, missions);
+                            if (state != "inProgress" && state != "Finished")
+                            {
+                                continue;
+                            }
+                            Mission mission = new Mission(codeName, state);
+                            commando.Missions.Add(mission);
+                        }
+                    }
                     army.Add(commando);
                     break;
-                case "spy":
+                case "Spy":
                     Spy spy = new Spy(soldierTokens[1], soldierTokens[2], soldierTokens[3], int.Parse(soldierTokens[4]));
                     army.Add(spy);
                     break;
@@ -66,40 +99,6 @@ class StartUp
         }
     }
 
-    private static void GetMissions(Commando commando, string[] missions)
-    {
-        for (int i = 0; i < missions.Length; i+= 2)
-        {
-            string codeName = missions[i];
-            string state = missions[i + 1];
-
-            if (state != "inProgress" && state != "Finished")
-            {
-                continue;
-            }
-
-            Mission mission = new Mission(codeName, state);
-
-            commando.Missions.Add(mission);
-        }
-    }
-
-    private static void GetRepairs(Engineer engineer, string[] repairs)
-    {
-        if (repairs.Any())
-        {
-            for (int i = 0; i < repairs.Length; i+= 2)
-            {
-                string partName = repairs[i];
-                int hoursWorked = int.Parse(repairs[i + 1]);
-
-                Repair repair = new Repair(partName, hoursWorked);
-
-                engineer.Repairs.Add(repair);
-            }
-        }
-    }
-
     private static bool CorpsIsValid(string corps)
     {
         if (corps == "Airforces" || corps == "Marines")
@@ -107,19 +106,5 @@ class StartUp
             return true;
         }
         return false;
-    }
-
-    private static void AddPrivates(List<Soldier> army, LeutenantGeneral officer, string[] privateIds)
-    {
-        foreach (string id in privateIds)
-        {
-            foreach (Soldier soldier in army)
-            {
-                if (soldier.Id == id)
-                {
-                    officer.Privates.Add(soldier);
-                }
-            }
-        }
     }
 }
